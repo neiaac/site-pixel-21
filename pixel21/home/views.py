75f26d239afc4d54b36e4be3689398823e10cb67
AlexandreTapadinhas <alexandre22tapadinhas@gmail.com>
2021-04-15 00:11:16 +0100

    Embed youtube video (opt1 - django)

--- pixel21/home/views.py
+++ pixel21/home/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
 
 from django.views.generic import View
+from .models import EmbedVideo
 
 # Create your views here.
 class homepage_view(View):
     def get(self, request):
         print("Homepage_get")
-        return render(request, 'homepage.html')
+        return render(request, 'index.html')
 
     def post(self, request):
         print("Homepage_post")
+
+
+def video(request):
+    video = EmbedVideo.objects.all()
+    return render(request, 'live.html', {'video':video})
